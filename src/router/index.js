4ebf11ba60991ce263e58cd20bfad3e9c6437c23
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-11 12:02:57 +0630

    home page and sign in page finish up

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignIn from '../views/SignIn.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/sign-in',
+    name: 'SignIn',
+    component: SignIn
   },
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <h1 class="text-blue-500">hello</h1>
+    <div class="rounded-lg p-12 bg-gray-200 text-center mt-8">
+      <h1 class="text-3xl font-medium text-gray-700 mb-3">UploaderCC 🥳</h1>
+      <p class="text-gray-500 mt-3">Instanly Upload Files,Share with anyone.</p>
+    </div>
   </div>
 </template>
 
