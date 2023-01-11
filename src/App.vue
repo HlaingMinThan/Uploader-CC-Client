a05e1d86803d2235e5695cefdb435df0282c10e7
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-11 12:17:39 +0630

    navbar component extraction

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="w-full md:w-8/12 lg:w-6/12 mx-auto mt-6 md:mt-20">
-  <nav class="flex justify-between items-center flex-wrap md:flex-no-wrap -mx-3 -mt-3">
-    <ul class="flex items-center">
-      <li><router-link to="/" class="text-sm inline-block  p-3 text-gray-800">Home</router-link></li>
-      <li><router-link to="" class="text-sm inline-block  p-3 text-gray-800">Your Files</router-link></li>
-    </ul>
-    <ul class="flex items-center">
-      <li><router-link to="/sign-in" class="text-sm inline-block  p-3 text-gray-800">Sign In</router-link></li>
-      <li><router-link to="" class="text-sm inline-block  p-3 text-gray-800">Create Account</router-link></li>
-    </ul>
-  </nav>
+  <Navbar></Navbar>
   <router-view/>
   </div>
 </template>
+<script>
+import Navbar from '@/components/Navbar';
+
+export default {
+  components : { Navbar }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="flex justify-between items-center flex-wrap md:flex-no-wrap -mx-3 -mt-3">
+    <ul class="flex items-center">
+      <li><router-link to="/" class="text-sm inline-block  p-3 text-gray-800">Home</router-link></li>
+      <li><router-link to="" class="text-sm inline-block  p-3 text-gray-800">Your Files</router-link></li>
+    </ul>
+    <ul class="flex items-center">
+      <li><router-link to="/sign-in" class="text-sm inline-block  p-3 text-gray-800">Sign In</router-link></li>
+      <li><router-link to="" class="text-sm inline-block  p-3 text-gray-800">Create Account</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
