bd680b77009070af07d6f46c63c8d0e46cd8d6a7
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-15 17:23:12 +0630

    user usage component implementation done

--- src/components/Usage.vue
+++ src/components/Usage.vue
@@ -1,14 +1,26 @@
 <template>
-    <span class="bg-gray-200 p-2  rounded-lg text-sm">Usage - {{usage}}/5MB (10%)</span>
+    <span class="bg-gray-200 p-1  rounded-lg text-sm">Usage - {{formattedUsage}}/ {{totalFormatForUser}} ({{percentage}}%)</span>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import {filesize} from 'filesize';
+
 export default {
     computed: {
         ...mapGetters({
+            user : "auth/user",
             usage : "usage/usage"
-        })
+        }),
+        formattedUsage(){
+            return filesize(this.usage);
+        },
+        totalFormatForUser() {
+            return filesize(this.user.plan.storage);
+        },
+        percentage() {
+            return ((this.usage / this.user.plan.storage) * 100).toFixed(2);
+        }
     },
     methods: {
         ...mapActions({
