3c69f9459e34faa4bc68d5bca4a3200350ed3b1d
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-18 17:40:11 +0630

    update ui stuff

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-between border-2 border-gray-200 py-4 px-4 rounded-xl my-3">
-        <h3 class="text-lg">{{$ucFirst(plan.name)}} - {{formattedStorage}}</h3>
+        <h3 class="text-lg"><span class="text-indigo-500 font-bold">{{$ucFirst(plan.name)}}</span> - {{formattedStorage}}</h3>
         <div class="text-xl text-indigo-500 font-bold">
             {{plan.price/100}} USD
         </div>
